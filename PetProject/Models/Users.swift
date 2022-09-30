17e6860ce4987cbce7679446a16f091fac755eb4
mrgsdev <mrgsdev@gmail.com>
2022-09-30 15:01:44 +0300

    .

--- PetProject/Models/Users.swift
+++ PetProject/Models/Users.swift
@@ -10,11 +10,13 @@ import Foundation
 struct Users {
     let username:String
     let email:String
+    
     var safeEmail:String{
         var safeEmail = email.replacingOccurrences(of: ".", with: "|")
         safeEmail = safeEmail.replacingOccurrences(of: "@", with: "*")
         return safeEmail
     }
+    
     var profilePictureFileName: String{
         return "\(safeEmail)_profile_pic.png"
     }
